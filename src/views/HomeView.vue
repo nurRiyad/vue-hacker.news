8ff5bd8de9372540c190a7b827b2e6212fab1488
nurRiyad <asadnurriyad@gmail.com>
2022-11-08 00:44:50 +0600

    remove boiler code
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>This is home page</h1>
+  </div>
 </template>
