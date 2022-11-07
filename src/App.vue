2a8b3c551923c5e0602a71e7498f9bf0d9d123c0
nurRiyad <asadnurriyad@gmail.com>
2022-11-08 00:35:16 +0600

    init project for vue3
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,85 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div id="wrapper">
-    <div id="nav">
-      <div>
-        <router-link to="/">Top</router-link>
-        <span id="divider">|</span>
-        <router-link to="/new">New</router-link>
-      </div>
-      <div id="name">
-        <p>HackerNewsDemo</p>
-      </div>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
-    <router-view />
-  </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
   text-align: center;
-  color: #2c3e50;
+  margin-top: 2rem;
 }
 
-#nav {
-  padding: 30px;
-  background-color: rgb(230, 113, 17);
-  padding-bottom: 10px;
-}
-#divider {
-  font-size: 20px;
-  margin: 10px;
+nav a.router-link-exact-active {
+  color: var(--color-text);
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  font-size: 20px;
-  border-bottom: 2px solid rgb(0, 0, 0);
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
 }
 
-#nav a.router-link-exact-active {
-  color: #ffffff;
-}
-
-#wrapper {
-  width: 90%;
-  max-width: 1000px;
-  margin: 20px auto;
-  border-radius: 6px;
-  box-shadow: 0px 1px 6px rgba(100, 70, 70, 0.2);
-  box-sizing: border-box;
-  padding: 0 0 20px;
-  overflow: hidden;
-  border: 1px solid lightgray;
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-}
-#page-banner {
-  font-size: 2rem;
-  text-align: center;
-  color: rgb(255, 255, 255);
-}
-ul {
-  list-style-type: none;
-  padding-inline-start: 0;
-}
-button {
-  text-decoration: none;
+nav a {
   display: inline-block;
-  padding: 8px 16px;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
   border: 0;
-  margin: 7px;
 }
-button:hover {
-  background-color: rgb(230, 113, 17);
-  color: black;
-}
-#name p {
-  margin-bottom: 0;
-  font-size: small;
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  lintOnSave: true,
-  runtimeCompiler: true,
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/hacker-news-demo/" : "/",
-};
