f8266e5cc14f2522b654e713d4e4313cfaa152e9
nurRiyad <asadnurriyad@gmail.com>
2022-11-24 00:58:09 +0600

    seperate newsitem
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

---
+++ src/components/NewItems.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex space-x-5  border-b-2  mt-1 mt-0">
+    <div class="flex justify-center items-center h-auto text-3xl p-5 font-sans font-semibold text-blue-700">
+      10
+    </div>
+    <div class="flex flex-col justify-center ">
+      <div class="flex items-center font-mono space-x-1">
+        <a href="" class="text-xl font-medium ">
+          This is news
+        </a>
+        <p class="text-sm text-slate-500">
+          (google.com)
+        </p>
+      </div>
+      <div class="flex text-sm text-slate-500">
+        <p>by Riyad</p>
+        <p class="px-4">
+          |
+        </p>
+        <p>1 day ago</p>
+      </div>
+    </div>
+  </div>
+</template>
