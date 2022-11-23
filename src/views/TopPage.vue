546f54d465efec888e753df37966dddc70e6cdc4
nurRiyad <asadnurriyad@gmail.com>
2022-11-24 00:58:09 +0600

    seperate newsitem
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/views/TopPage.vue
+++ src/views/TopPage.vue
@@ -1,33 +1,12 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
+import { useFetch } from '@vueuse/core'
 import { useNewStore } from '@/stores/news'
-
-const newsStore = useNewStore()
+import NewItems from '@/components/NewItems.vue'
 </script>
 
 <template>
-  <div class="w-full bg-white space-y-5">
-    <div class="flex space-x-5 border-blue-400 border-b-4  mt-2">
-      <div class="flex justify-center items-center h-auto text-3xl p-5 font-sans font-semibold text-blue-700">
-        10
-      </div>
-      <div class="flex flex-col justify-center ">
-        <div class="flex items-center font-mono space-x-1">
-          <a href="" class="text-xl font-medium ">
-            This is news
-          </a>
-          <p class="text-sm text-slate-500">
-            (google.com)
-          </p>
-        </div>
-        <div class="flex text-sm text-slate-500">
-          <p>by Riyad</p>
-          <p class="px-4">
-            |
-          </p>
-          <p>1 day ago</p>
-        </div>
-      </div>
-    </div>
+  <div class="w-full bg-white">
+    <NewItems v-for="n in 10" :key="n" />
   </div>
 </template>
