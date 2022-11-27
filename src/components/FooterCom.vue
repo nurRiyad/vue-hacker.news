dc6e8c2fda7581a391c26fd318126245cf3b2b47
nurRiyad <asadnurriyad@gmail.com>
2022-11-28 01:23:10 +0600

    update news items
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/components/FooterCom.vue
+++ src/components/FooterCom.vue
@@ -7,12 +7,14 @@ import LinkedIn from './icons/LinkedIn.vue'
   <div class="py-2 bg-blue-800 text-gray-300 font-mono">
     <div class="flex flex-row justify-center container mx-auto max-w-5xl space-x-5">
       <div class="flex justify-center items-center space-x-2">
-        <GithubVue :width="20" />
-        <p>nurRiyad</p>
+        <a href="http://github.com/nurriyad">
+          <GithubVue :width="20" />
+        </a>
       </div>
       <div class="flex justify-center items-center space-x-2">
-        <LinkedIn :width="18" />
-        <p>nurRiyad</p>
+        <a href="http://linkedin/nurriyad">
+          <LinkedIn :width="18" />
+        </a>
       </div>
     </div>
   </div>
---
+++ src/types/News.ts
@@ -0,0 +1,10 @@
+export interface News {
+  by: string
+  id: number
+  kids: Array<number>
+  score: number
+  time: number
+  title: string
+  type: string
+  url: string
+}
