26ee42fd2d1aee542c99b481173e7caec9bb3e4d
nurRiyad <asadnurriyad@gmail.com>
2022-11-27 23:58:46 +0600

    add news api call
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/components/NewItems.vue
+++ src/components/NewItems.vue
@@ -1,23 +1,50 @@
+<script setup lang="ts">
+import { computed } from '@vue/reactivity'
+import { useFetch } from '@vueuse/core'
+import { onMounted, ref } from 'vue'
+
+// set props
+interface Props {
+  id: number
+}
+const props = withDefaults(defineProps<Props>(), {
+  id: NaN,
+})
+
+const title = ref('no title')
+const score = ref(0)
+const author = ref('no author available')
+const time = ref(0)
+
+const { data: resp } = await useFetch(`https://hacker-news.firebaseio.com/v0/item/${props.id}.json?print=pretty`)
+const news = ref(JSON.parse(resp.value as string))
+
+title.value = news.value.title
+score.value = news.value.score
+author.value = news.value.by
+time.value = news.value.time
+</script>
+
 <template>
   <div class="flex space-x-5  border-b-2  mt-0">
-    <div class="flex justify-center items-center h-auto text-3xl p-5 font-sans font-semibold text-blue-700">
-      10
+    <div class="flex justify-center items-center h-auto p-5 font-sans font-semibold text-blue-700">
+      {{ score }}
     </div>
     <div class="flex flex-col justify-center ">
       <div class="flex items-center font-mono space-x-1">
-        <a href="" class="text-xl font-medium ">
-          This is news
+        <a href="" class="font-medium ">
+          {{ title }}
         </a>
         <p class="text-sm text-slate-500">
           (google.com)
         </p>
       </div>
       <div class="flex text-sm text-slate-500">
-        <p>by Riyad</p>
+        <p>by {{ author }}</p>
         <p class="px-4">
           |
         </p>
-        <p>1 day ago</p>
+        <p>{{ time }} ago</p>
       </div>
     </div>
   </div>
--- src/views/TopPage.vue
+++ src/views/TopPage.vue
@@ -1,14 +1,46 @@
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue'
-import { useFetch } from '@vueuse/core'
+import { storeToRefs } from 'pinia'
 import { useNewStore } from '@/stores/news'
 import NewItems from '@/components/NewItems.vue'
+
+const newsStore = useNewStore()
+const { topNewsList } = storeToRefs(newsStore)
+
+const currentPage = ref(1)
+const totalPage = computed(() => {
+  const totalNews = topNewsList.value.length
+  if (totalNews % 20 === 0)
+    return totalNews / 20
+  else return Math.floor(totalNews / 20) + 1
+})
+
+const filteredNews = computed(() => {
+  const fst = (currentPage.value - 1) * 20
+  const lst = (currentPage.value * 20) - 1
+
+  return topNewsList.value.filter((val, idx) => {
+    if (idx <= lst && idx >= fst)
+      return true
+    else return false
+  })
+})
 </script>
 
 <template>
-  <div class="flex flex-col bg-white">
-    <h1 class="flex-grow">
-      <NewItems />
-    </h1>
+  <div class="flex flex-col bg-white h-full w-full">
+    <div v-for="n in filteredNews" :key="n" class="flex-grow">
+      <Suspense>
+        <template #default>
+          <NewItems :id="n" />
+        </template>
+        <template #fallback>
+          Loading...
+        </template>
+      </Suspense>
+    </div>
+    <div class="text-center my-2 py-2 ">
+      <p> {{ currentPage }}/{{ totalPage }}</p>
+    </div>
   </div>
 </template>
