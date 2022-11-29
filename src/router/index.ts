c3a4717708dbead35063432af798ae5ea77b10f8
nurRiyad <asadnurriyad@gmail.com>
2022-11-29 21:39:56 +0600

    add content for best & new news
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import NewPage from '@/views/NewPage.vue'
 import ShowPage from '@/views/ShowPage.vue'
 import AskPage from '@/views/AskPage.vue'
 import JobsPage from '@/views/JobsPage.vue'
+import BestPage from '@/views/BestPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,11 @@ const router = createRouter({
       path: '/new',
       name: 'newPage',
       component: NewPage,
+    },
+    {
+      path: '/best',
+      name: 'bestPage',
+      component: BestPage,
     },
     {
       path: '/show',
