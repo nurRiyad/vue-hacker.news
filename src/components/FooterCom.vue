ce540cca48b0d022ff7f2b9d41a818aaa5b6b18d
nurRiyad <asadnurriyad@gmail.com>
2023-01-14 14:50:18 +0600

    make navbar & footer responsive
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/components/FooterCom.vue
+++ src/components/FooterCom.vue
@@ -4,18 +4,17 @@ import LinkedIn from './icons/LinkedIn.vue'
 </script>
 
 <template>
-  <div class="py-2  bg-sky-700 text-gray-300 font-mono">
-    <div class="flex flex-row justify-center container mx-auto max-w-6xl space-x-5">
-      <div class="flex justify-center items-center space-x-2">
+  <div class="py-2  bg-[#f3621d] text-white font-mono">
+    <div class="flex items-center justify-center container mx-auto max-w-6xl space-x-5">
         <a href="http://github.com/nurriyad">
           <GithubVue :width="20" />
         </a>
-      </div>
-      <div class="flex justify-center items-center space-x-2">
         <a href="http://linkedin/nurriyad">
           <LinkedIn :width="18" />
         </a>
-      </div>
+        <a href="http://github.com/nurriyad/hacker-news" target="_blank" class="hover:font-semibold underline">
+          Source Code
+        </a>
     </div>
   </div>
 </template>
--- src/components/NavbarCom.vue
+++ src/components/NavbarCom.vue
@@ -8,37 +8,42 @@ const routeName = computed(() => route.name || '')
 </script>
 
 <template>
-  <div class="p-5 bg-sky-700 text-gray-300 font-mono">
-    <div class="flex flex-row justify-between container mx-auto max-w-6xl">
+  <div class="p-5 bg-[#f3621d] text-white font-mono">
+    <div class="flex flex-row justify-between container mx-auto max-w-6xl px-2">
       <!-- Left navbar -->
       <div class="flex flex-row space-x-4">
         <RouterLink to="/">
-          <img class="w-10" src="@/assets/logo/hackerNews.png" alt="hacker new logo">
+          <!-- <img class="w-10" src="@/assets/logo/hackerNews.png" alt="hacker new logo"> -->
+          <div class="flex">
+            <img class="sm:hidden w-10 object-cover
+            " src="@/assets/logo/hackerNews.png" alt="hackerNew logo">
+            <p class="hidden text-xl font-bold sm:block" :class="{ underline: routeName === 'topNews' }">HackerNews</p>
+          </div>
         </RouterLink>
-        <RouterLink to="/" class="hover:text-white hover:font-semibold" :class="{ underline: routeName === 'topNews' }">
-          Top
-        </RouterLink>
-        <RouterLink to="/new" class="hover:text-white hover:font-semibold" :class="{ underline: routeName === 'newPage' }">
-          New
-        </RouterLink>
-        <RouterLink to="/best" class="hover:text-white hover:font-semibold" :class="{ underline: routeName === 'bestPage' }">
-          Best
-        </RouterLink>
-        <RouterLink to="/show" class="hover:text-white hover:font-semibold" :class="{ underline: routeName === 'showPage' }">
-          Show
-        </RouterLink>
-        <RouterLink to="/ask" class="hover:text-white hover:font-semibold" :class="{ underline: routeName === 'askPage' }">
-          Ask
-        </RouterLink>
-        <RouterLink to="/jobs" class="hover:text-white hover:font-semibold" :class="{ underline: routeName === 'jobsPage' }">
-          Jobs
-        </RouterLink>
+
       </div>
       <!-- Right Navbar -->
-      <div class="flex flex-row space-x-4">
-        <a href="https://nurriyad.xyz" target="_blank" class="hover:text-white hover:font-semibold hover:underline">
-          Built by Riyad
-        </a>
+      <div class="flex flex-row space-x-4 ">
+        <RouterLink to="/new" class="hover:text-white hover:font-semibold"
+          :class="{ underline: routeName === 'newPage' }">
+          New
+        </RouterLink>
+        <RouterLink to="/best" class="hover:text-white hover:font-semibold"
+          :class="{ underline: routeName === 'bestPage' }">
+          Best
+        </RouterLink>
+        <RouterLink to="/show" class="hover:text-white hover:font-semibold"
+          :class="{ underline: routeName === 'showPage' }">
+          Show
+        </RouterLink>
+        <RouterLink to="/ask" class="hover:text-white hover:font-semibold"
+          :class="{ underline: routeName === 'askPage' }">
+          Ask
+        </RouterLink>
+        <RouterLink to="/jobs" class="hover:text-white hover:font-semibold"
+          :class="{ underline: routeName === 'jobsPage' }">
+          Jobs
+        </RouterLink>
       </div>
     </div>
   </div>
