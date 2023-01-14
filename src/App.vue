0a5592b04b83d27b169ef320d31e8ca4ebca0b8b
nurRiyad <asadnurriyad@gmail.com>
2023-01-14 16:45:11 +0600

    add metaTag with vueUse
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,74 @@ import { onMounted } from 'vue'
 import FooterCom from '@/components/FooterCom.vue'
 import NavbarCom from '@/components/NavbarCom.vue'
 import { useNewStore } from '@/stores/news'
+import { useHead } from '@unhead/vue'
 
 const newsStore = useNewStore()
 onMounted(() => {
   newsStore.fetchAllNews()
 })
+
+// add meta tags
+const description =
+  'Hacker News is a social news website focusing on computer science and entrepreneurship. This site with built by Al Asad Nur Riyad wiht Vue3, Vue-Router, Pinia, TailwindCss, VueUse'
+const ogTitle =
+  'HackerHews built with Vue3, Vue-Router, Pinia, TailwindCss, VueUse By Al Asad Nur Riyad'
+const twitterDescription =
+  'HackerHews built with Vue3, Vue-Router, Pinia, TailwindCss, VueUse By Al Asad Nur Riyad'
+const image =
+  'https://hnews-nurriyad.netlify.app/android-chrome-512x512.png'
+const mySite = 'https://hnews-nurriyad.netlify.app'
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    {
+      name: 'description',
+      content: description,
+    },
+    // Test on: https://developers.facebook.com/tools/debug/ or https://socialsharepreview.com/
+    { property: 'og:site_name', content: mySite },
+    { property: 'og:type', content: 'website' },
+    {
+      property: 'og:url',
+      content: mySite,
+    },
+    {
+      property: 'og:title',
+      content: ogTitle,
+    },
+    {
+      property: 'og:description',
+      content: description,
+    },
+    {
+      property: 'og:image',
+      content: image,
+    },
+    // Test on: https://cards-dev.twitter.com/validator or https://socialsharepreview.com/
+    { name: 'twitter:site', content: '@debs_obrien' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    {
+      name: 'twitter:url',
+      content: mySite,
+    },
+    {
+      name: 'twitter:title',
+      content: ogTitle,
+    },
+    {
+      name: 'twitter:description',
+      content: twitterDescription,
+    },
+    {
+      name: 'twitter:image',
+      content: image,
+    },
+  ],
+})
+
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { createHead } from "@vueuse/head"
+
+
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
 const app = createApp(App)
+const head = createHead()
 
 app.use(createPinia())
 app.use(router)
+app.use(head)
 
 app.mount('#app')
